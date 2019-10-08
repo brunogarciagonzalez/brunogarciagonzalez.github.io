a5d4ed6536b0e9f99d2907ec88d39abac54d1195
Bruno Garcia Gonzalez <brunogarciagonzalez@outlook.com>
2019-10-08 12:20:00 -0400

    add BookSwap, try 2

---
+++ precache-manifest.8e604441c884fc612e05361014cba444.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c2f43cbded08f49e79b",
+    "url": "/static/css/main.fb82606e.chunk.css"
+  },
+  {
+    "revision": "5c2f43cbded08f49e79b",
+    "url": "/static/js/main.5c2f43cb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "621473413dace574c5fe",
+    "url": "/static/js/2.62147341.chunk.js"
+  },
+  {
+    "revision": "4e078cdf356522d4d4f9098f8e1c296b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.abc187f98b0080402a4848b44fbeeee2.js"
+  "/precache-manifest.8e604441c884fc612e05361014cba444.js"
 );
 
 workbox.clientsClaim();
