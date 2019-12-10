58676a39f0fd68a86d4f4e2e298f1346f1b95b71
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2019-12-10 14:58:26 -0500

    add NewsChat

--- precache-manifest.94e6f661d06c78576afebc0a798cf1b7.js
+++ precache-manifest.94e6f661d06c78576afebc0a798cf1b7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "25640a63bdcf4e419d00",
-    "url": "/static/css/main.bcd53d02.chunk.css"
+    "revision": "58459de9298fda2b0462",
+    "url": "/static/css/main.960a0dca.chunk.css"
   },
   {
-    "revision": "25640a63bdcf4e419d00",
-    "url": "/static/js/main.25640a63.chunk.js"
+    "revision": "58459de9298fda2b0462",
+    "url": "/static/js/main.58459de9.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/2.deee32e4.chunk.js"
   },
   {
-    "revision": "00c8e1c77b21c498030923faf87f3da7",
+    "revision": "069d54170aa320cd0eb5898612720129",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e627194f98d3942a6fef7b2018960d82.js"
+  "/precache-manifest.94e6f661d06c78576afebc0a798cf1b7.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
